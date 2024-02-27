546ec53374d9a5aba62b550ac047db2b1bead16f
nik <n.vasyankin@vvdev.ru>
2024-02-27 23:55:25 +0300

    test

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,33 +1,37 @@
-import { Button, FileButton, Group, Progress } from '@mantine/core'
-import { useEffect } from 'react'
-
-const downLoadHandler = () => {
-	const eventSource = new EventSource(
-		'http://localhost:3030/upload-movies/progress',
-	)
-
-	eventSource.onerror = function (err) {
-		console.error('EventSource failed:', err)
-	}
-
-	eventSource.onmessage = function (event) {
-		const data = JSON.parse(event.data)
-		const progress = data.progress
-		console.log('Upload progress:', progress)
-	}
-}
+import { Button, FileButton, Progress } from '@mantine/core'
+import { useCallback, useEffect, useState } from 'react'
 
 export const App = () => {
-	const sendFile = (file: File) => {
-		downLoadHandler()
+	const [value, setValue] = useState(0)
 
+	const sendFile = useCallback((file: File) => {
 		const formData = new FormData()
 		formData.append('file', file)
 		fetch('http://localhost:3030/upload-movies', {
 			method: 'POST',
 			body: formData,
 		})
-	}
+	}, [])
+
+	useEffect(() => {
+		const eventSource = new EventSource(
+			'http://localhost:3030/upload-movies/progress',
+		)
+
+		eventSource.addEventListener('error', (err) => {
+			console.error('EventSource failed:', err)
+		})
+
+		eventSource.addEventListener('message', (event) => {
+			const data = JSON.parse(event.data)
+			const progress = data.progress
+			setValue(progress)
+
+			if (progress === 100) {
+				eventSource.close()
+			}
+		})
+	}, [sendFile])
 
 	return (
 		<>
@@ -35,6 +39,9 @@ export const App = () => {
 			<FileButton onChange={sendFile} accept="video/*">
 				{(props) => <Button {...props}>Upload image</Button>}
 			</FileButton>
+			<div style={{ padding: '10px', width: 600 }}>
+				<Progress value={value} size="lg" transitionDuration={200} />
+			</div>
 		</>
 	)
 }
